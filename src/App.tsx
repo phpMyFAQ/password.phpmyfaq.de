a6515a6d2302e818ae2b971b9a27d1ec7b740594
Thorsten Rinne <thorsten@phpmyfaq.de>
2019-12-12 20:58:03 +0100

    Added some content

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,11 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Password hash generator for phpMyFAQ
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
