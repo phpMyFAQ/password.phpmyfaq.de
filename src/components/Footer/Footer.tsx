fd71cd9a02c09ef64257cd09d87560891f61fbb9
Thorsten Rinne <thorsten@phpmyfaq.de>
2020-03-15 08:24:31 +0100

    chore: bump copyright year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="my-2 pt-2 text-muted text-center text-small">
       Made with <span role="img" aria-label="heart">❤</span>️ and <span role="img" aria-label="coffee">☕️</span>
       <br/>
-      <span role="img">©</span> 2019 Thorsten Rinne
+      <span role="img">©</span> 2019 - 2020 Thorsten Rinne
     </footer>
   </>
 }
