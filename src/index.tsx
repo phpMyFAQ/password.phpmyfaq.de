04aede419a667c9026cba15a02f2e2dc0c297ff5
Thorsten Rinne <thorsten@phpmyfaq.de>
2022-02-19 11:15:20 +0100

    feat: removed old SW

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import { App } from './components/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.register();
