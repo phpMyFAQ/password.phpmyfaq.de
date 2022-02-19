f06d2588faba6316198886d038e4d4ee8f23cd04
Thorsten Rinne <thorsten@phpmyfaq.de>
2022-02-19 11:07:21 +0100

    feat: added link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,9 @@ export const Footer: FunctionComponent = () => {
     <footer className="my-2 pt-2 text-muted text-center text-small">
       Made with <span role="img" aria-label="heart">❤</span>️ and <span role="img" aria-label="coffee">☕️</span>
       <br/>
-      <span role="img">©</span> 2019 - 2022 Thorsten Rinne
+      <span role="img">©</span> 2019 - 2022 <a target={'_blank'} rel={'noreferrer'} href={'https://rinne.info'}>
+        Thorsten Rinne
+    </a>
     </footer>
   </>
 }
